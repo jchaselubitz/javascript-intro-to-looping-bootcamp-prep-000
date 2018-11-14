52b50fac5b9d83f3aa7a9321be51828039f6fbb6
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 23:03:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 0) {
+    if (i === 1) {
       return array.push("I am ${i} strange loop.")
     } else {
       return array.push("I am ${i} strange loops.")
     }
   }
+  return array
 }
